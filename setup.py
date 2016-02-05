ee09301f3490913819b7692d881b37045b28f7a8
Andrew Post <andrew.post55@gmail.com>
2016-02-05 14:02:40 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(description='Eureka! Python Client',
+      author='The Eureka Clinical team',
+      url='http://eurekaclinical.org',
+      download_url='Where to download it.',
+      author_email='help@eurekaclinical.org',
+      version='0.1',
+      install_requires=[],
+      packages=[],
+      scripts=[],
+      name='eurekaclinical'
+}
