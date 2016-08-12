e5053231cb71c61a843c90769bd17839add25cbe
Andrew Post <arpost@emory.edu>
2016-08-12 18:54:43 -0400

    Added to setup.py.

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(description='Eureka! Python Client',
+setup(description='Eureka! Clinical Python Client',
       author='The Eureka Clinical team',
       url='http://eurekaclinical.org',
       download_url='Where to download it.',
       author_email='help@eurekaclinical.org',
-      version='0.1',
-      install_requires=[],
-      packages=[],
-      scripts=[],
-      name='eurekaclinical'
-}
+      version='1.0.a1',
+      license='Apache License 2.0',
+      install_requires=['requests'],
+      #python_requires='>= 2.7.10',
+      packages=find_packages(),
+      name='eurekaclinical',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 2.7'
+          'License :: OSI Approved :: Apache Software License'
+          'Programming Language :: Python :: Implementation :: CPython'
+      ]
+)
